44e47bc26d487d7c6ea3f908e2172e3409bd6531
Jörg Pichler <joerg.pichler@gmail.com>
2020-12-04 18:37:35 +0100

    Day 3 Part 1

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -13,102 +13,173 @@ namespace Day3
         {
             var lines = Assembly.GetExecutingAssembly().GetEmbeddedResourceLines("Day3.input.txt").ToList();
 
-            var map = TreeMap.Parse(lines);
+            var wire1 = GetVectors(lines[0]);
+            var wire2 = GetVectors(lines[1]);
 
-            Part1(map);
+            Part1(wire1, wire2);
 
             Console.WriteLine();
 
-            Part2(map);
+            Part2(wire1, wire2);
         }
 
-        private static void Part1(TreeMap treeMap)
+        private static void Part1(List<Vector> wire1, List<Vector> wire2)
         {
             ConsoleHelper.Part1();
 
-            Console.WriteLine($"{treeMap.GetNoOfEncounteredTrees(3, 1)} trees found");
+            List<Point> intersectionPoints = new List<Point>();
+
+            foreach (var wire1Vector in wire1)
+            {
+                intersectionPoints.AddRange(wire2.Select(v => v.Intersects(wire1Vector)).Where(x => x != null));
+            }
+
+            var shortestDistance = intersectionPoints.Select(x => x.X + x.Y).Min();
+
+            Console.WriteLine($"Closest intersection distance: {shortestDistance}");
         }
 
-        private static void Part2(TreeMap treeMap)
+        private static void Part2(List<Vector> wire1, List<Vector> wire2)
         {
-            ConsoleHelper.Part2();
-
-            var result = treeMap.GetNoOfEncounteredTrees(1, 1) * treeMap.GetNoOfEncounteredTrees(3, 1) *
-                         treeMap.GetNoOfEncounteredTrees(5, 1) * treeMap.GetNoOfEncounteredTrees(7, 1) * treeMap.GetNoOfEncounteredTrees(1, 2);
-
-            Console.WriteLine($"Multiplication result is {result}");
+            throw new NotImplementedException();
         }
+
+        private static List<Vector> GetVectors(string line)
+        {
+            var result = new List<Vector>();
+
+            var directions = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            for (var i = 0; i < directions.Length; i++)
+            {
+                var direction = directions[i];
+
+                var vector = new Vector();
+                if (i == 0)
+                {
+                    vector.From = new Point(0, 0);
+                }
+                else
+                {
+                    vector.From = (Point) result[i - 1].To.Clone();
+                }
+
+                vector.To = (Point) vector.From.Clone();
+
+                var x = direction[0];
+                var l = int.Parse(direction.Substring(1));
+
+                switch (x)
+                {
+                    case 'L':
+                        vector.To.X -= l;
+                        break;
+                    case 'R':
+                        vector.To.X += l;
+                        break;
+                    case 'U':
+                        vector.To.Y += l;
+                        break;
+                    case 'D':
+                        vector.To.Y -= l;
+                        break;
+                }
+
+                result.Add(vector);
+            }
+
+            return result;
+        }
+
     }
 
     [DebuggerDisplay("{X},{Y}")]
-    internal class Point
+    internal class Point : ICloneable
     {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
         public int X { get; set; }
 
         public int Y { get; set; }
 
-        public void Add(int x, int y)
+        public object Clone()
         {
-            X += x;
-            Y += y;
+            return new Point(X, Y);
+        }
+
+        protected bool Equals(Point other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Point) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
         }
     }
 
-    internal class TreeMap
+    [DebuggerDisplay("{From},{To}")]
+    internal class Vector
     {
-        private readonly char[,] _map;
-
-        private TreeMap(char[,] map)
+        public Vector()
         {
-            _map = map;
         }
 
-        public bool Contains(Point point)
+        public Vector(int x1, int y1, int x2, int y2)
         {
-            return point.Y < _map.GetLength(1);
+            From = new Point(x1, y1);
+            To = new Point(x2, y2);
         }
 
-        public char Get(int x, int y)
-        {
-            return _map[x % _map.GetLength(0), y];
-        }
+        public Point From { get; set; }
 
-        public static TreeMap Parse(List<string> lines)
-        {
-            char[,] map = new char[lines[0].Length, lines.Count];
+        public Point To { get; set; }
 
-            for (var y = 0; y < lines.Count; y++)
+        public bool IsHorizontal => From.Y == To.Y;
+
+        public bool IsVertical => From.X == To.X;
+
+        public Point Intersects(Vector vector)
+        {
+            if (IsHorizontal && vector.IsVertical)
             {
-                for (var x = 0; x < lines[y].Length; x++)
+                if (vector.From.X >= MinX && vector.From.X <= MaxX &&
+                    vector.MinY <= From.Y && vector.MaxY >= From.Y)
                 {
-                    map[x, y] = lines[y][x];
+                    return new Point(vector.From.X, From.Y);
                 }
             }
 
-            return new TreeMap(map);
-        }
-    }
-
-    internal static class TreeMapExtensions
-    {
-        public static int GetNoOfEncounteredTrees(this TreeMap treeMap, int slopeX, int slopeY)
-        {
-            var coords = new Point { X = 0, Y = 0 };
-            coords.Add(slopeX, slopeY);
-
-            int treeCtr = 0;
-
-            while (treeMap.Contains(coords))
+            if (IsVertical && vector.IsHorizontal)
             {
-                if (treeMap.Get(coords.X, coords.Y) == '#')
+                if (vector.From.Y >= MinY && vector.From.Y <= MaxY &&
+                    vector.MinX <= From.X && vector.MaxX >= From.X)
                 {
-                    treeCtr += 1;
+                    return new Point(From.X, vector.From.Y);
                 }
-
-                coords.Add(slopeX, slopeY);
             }
 
-            return treeCtr;
+            return null;
         }
+
+        private int MinX => Math.Min(From.X, To.X);
+
+        private int MinY => Math.Min(From.Y, To.Y);
+
+        private int MaxX => Math.Max(From.X, To.X);
+
+        private int MaxY => Math.Max(From.Y, To.Y);
     }
 }
