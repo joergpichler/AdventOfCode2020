c068f51cfe587f713f331e1405f54a4849942245
Jörg Pichler <joerg.pichler@gmail.com>
2020-12-16 09:13:03 +0100

    Day 13 Part 2 (thx Reddit)

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -14,12 +14,16 @@ namespace Day13
 
             var earliestDeparture = int.Parse(lines[0]);
             var busSchedules = lines[1].Split(',', StringSplitOptions.RemoveEmptyEntries)
-                .Where(s => int.TryParse(s, out _)).Select(s => int.Parse(s)).ToList();
+                .Select(s => int.TryParse(s, out var x) ? x : -1).ToArray();
 
-            Part1(earliestDeparture, busSchedules);
+            Part1(earliestDeparture, busSchedules.Where(x => x != -1));
+
+            Console.WriteLine();
+
+            Part2(busSchedules);
         }
 
-        private static void Part1(int earliestDeparture, List<int> busSchedules)
+        private static void Part1(int earliestDeparture, IEnumerable<int> busSchedules)
         {
             var scheduleAndRemainder = busSchedules.Select(b =>
             {
@@ -40,6 +44,39 @@ namespace Day13
 
             ConsoleHelper.Part1();
             Console.WriteLine($"Bus ID: {min.busSchedule} Departure: {min.departure} Wait time: {waitTime} Result: {min.busSchedule * waitTime}");
+        }
+
+        private static void Part2(IReadOnlyList<int> busSchedules)
+        {
+            IList<(int index, int busSchedule)> schedules = new List<(int index, int busSchedule)>();
+
+            for (var i = 0; i < busSchedules.Count; i++)
+            {
+                var busSchedule = busSchedules[i];
+                if (busSchedule == -1)
+                {
+                    continue;
+                }
+                schedules.Add((i, busSchedule));
+            }
+
+            ulong time = 0;
+            ulong multiplier = (ulong) schedules[0].busSchedule;
+            int satisfied = 1;
+
+            while (satisfied < schedules.Count)
+            {
+                time += multiplier;
+                var next = schedules[satisfied];
+
+                if ((time + (ulong) next.index) % (ulong)next.busSchedule == 0)
+                {
+                    multiplier *= (ulong) next.busSchedule;
+                    satisfied++;
+                }
+            }
+            
+            Console.WriteLine($"Timestamp: {time}");
         }
     }
 }
